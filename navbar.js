ab999176dec0e420e347d4316da64e074eb26330
lanetb <lanetb@dukes.jmu.edu>
2023-02-26 23:17:21 -0500

    nav bar logo finished

--- navbar.js
+++ navbar.js
@@ -3,15 +3,19 @@ const toggle = document.querySelector('.toggle-btn');
 const toggleIcon = document.querySelector('.toggle-btn i');
 const dropDown = document.querySelector('.drop-down');
 const drawLogo = document.querySelector('.name-mini');
+const spacer = document.querySelector('.spacer');
 
 window.addEventListener('scroll', function() {
     if (window.scrollY > 400) {
         nav.classList.add('nav-scroll');
         dropDown.style.setProperty('background','rgba(0, 0, 0, .15)');
-        drawLogo.style.setProperty('visablity','visible');
+        drawLogo.classList.remove('hidden');
+        spacer.classList.add('hidden');
     } else if (window.scrollY <= 400){
         nav.classList.remove('nav-scroll');
         dropDown.style.setProperty('background','rgba(255, 255, 255, .15)');
+        drawLogo.classList.add('hidden');
+        spacer.classList.remove('hidden');
     }
 });
 
