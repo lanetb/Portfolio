a2ae10ac28d03d46cf451506761b4982387ae74d
lanetb <lanetb@dukes.jmu.edu>
2023-02-26 21:47:38 -0500

    added break

--- navbar.js
+++ navbar.js
@@ -2,11 +2,13 @@ const nav = document.querySelector('nav');
 const toggle = document.querySelector('.toggle-btn');
 const toggleIcon = document.querySelector('.toggle-btn i');
 const dropDown = document.querySelector('.drop-down');
+const drawLogo = document.querySelector('.name-mini');
 
 window.addEventListener('scroll', function() {
     if (window.scrollY > 400) {
         nav.classList.add('nav-scroll');
         dropDown.style.setProperty('background','rgba(0, 0, 0, .15)');
+        drawLogo.style.setProperty('visablity','visible');
     } else if (window.scrollY <= 400){
         nav.classList.remove('nav-scroll');
         dropDown.style.setProperty('background','rgba(255, 255, 255, .15)');
